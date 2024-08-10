1d8ca8d6c5be1686ffb2d82a51c90ac2439bb3e3
Denny Wright <dennyw17@gmail.com>
2024-08-10 17:33:15 -0600

    WIP Grimoire

--- data/grimoire.py
+++ data/grimoire.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
 import json
+import re
 
 
 def main():
     with open("grimoire.txt") as fp:
-        lines = fp.readlines()
+        lines = [line for line in fp.readlines() if line.strip()]
 
     for line in lines:
-        if not line:
-            continue
         if line.startswith("#"):
             continue
-        print(line)
+        match = re.match("[A-Z ]+", line)
+        if match:
+            print(line)
 
     return
 
